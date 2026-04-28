6a8c4969a085f8f9c0332b8a0266368c61576af3
MariaJulia <mariajuliadev@gmail.com>
2026-04-28 09:36:50 -0300

    Métodos para identificar registrador

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,21 @@
 import entity.Montador;
 
 import java.util.List;
+import java.util.Map;
 
 public class Main {
+
     public static void main(String[] args) {
 
-        List<String> linhas = Montador.lerArquivo("filePath");
-        for (int i = 0; i < linhas.size(); i++){
-            System.out.println((i + 1) + ": " + linhas.get(i));
+        String caminho = "C:\\Dev\\UFERSA\\3 Semestre\\Arq. e Org. de Comp\\testeMontador.asm";
+
+        // ETAPA 1 + 2 (já estão juntas no seu código)
+        List<String> linhas = Montador.lerArquivo(caminho);
+        
+        // Verificar se houve erro na leitura do arquivo
+        if (linhas == null) {
+            System.err.println("Falha crítica: não foi possível ler o arquivo. Encerrando programa.");
+            System.exit(1);
         }
 
     }
--- src/entity/Montador.java
+++ src/entity/Montador.java
@@ -30,6 +30,7 @@ public class Montador {
             }
         }catch (IOException e){
             System.out.println("Erro ao ler arquivo: " + e.getMessage());
+            return null;
         }
 
         return linhas;
@@ -47,6 +48,7 @@ public class Montador {
                 tabela.put(partes[0].trim(), contadorInstrucoes);
 
                 if(partes.length > 1 && !partes[1].trim().isEmpty()){
+                    linhas.set(i, partes[1].trim());
                     contadorInstrucoes++;
                 }else{
                     linhas.set(i, "");
@@ -59,6 +61,67 @@ public class Montador {
 
         linhas.removeIf(String::isEmpty);
         return tabela;
+    }
+
+    public static Map<String, Integer> criarMapaRegistradores(){
+        Map<String, Integer> reg = new HashMap<>();
+
+        reg.put("$zero", 0);
+        reg.put("$at", 1);
+        reg.put("$v0", 2);
+        reg.put("$v1", 3);
+
+        reg.put("$a0", 4);
+        reg.put("$a1", 5);
+        reg.put("$a2", 6);
+        reg.put("$a3", 7);
+
+        reg.put("$t0", 8);
+        reg.put("$t1", 9);
+        reg.put("$t2", 10);
+        reg.put("$t3", 11);
+        reg.put("$t4", 12);
+        reg.put("$t5", 13);
+        reg.put("$t6", 14);
+        reg.put("$t7", 15);
+
+        reg.put("$s0", 16);
+        reg.put("$s1", 17);
+        reg.put("$s2", 18);
+        reg.put("$s3", 19);
+        reg.put("$s4", 20);
+        reg.put("$s5", 21);
+        reg.put("$s6", 22);
+        reg.put("$s7", 23);
+
+        reg.put("$t8", 24);
+        reg.put("$t9", 25);
+
+        reg.put("$gp", 28);
+        reg.put("$sp", 29);
+        reg.put("$fp", 30);
+        reg.put("$ra", 31);
+
+        return reg;
+    }
+
+    public static int getNumeroRegstrador(String reg, Map<String, Integer> tabelaReg){
+        reg = reg.trim();
+
+        if(reg.matches("\\$\\d+")){
+            int numero = Integer.parseInt(reg.substring(1));
+
+            if(numero >= 0 && numero <= 31){
+                return numero;
+            }
+        }
+
+        if(tabelaReg.containsKey(reg)){
+            return tabelaReg.get(reg);
+        }
+
+        System.out.println("Registrador inválido: " + reg);
+        return -1;
     }
 
 }
